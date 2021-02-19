d4237e5cd6475b1d5f7e4efd59adc5c1d9b715aa
HarryEazy <>
2021-02-19 20:35:15 +0000

    Add try block

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
 
-user_word = input(str("Enter word: ")).upper()
-code_list = [nato_dict[letter] for letter in user_word if letter in nato_dict.keys()]
 
-print(code_list)
+def generate_phonetic_alphabet():
 
+    user_word = input("Enter word: ").upper()
+    try:
+        code_list = [nato_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry only letter in the alphabet please")
+        generate_phonetic_alphabet()
+    else:
+        print(code_list)
+
+
+
+generate_phonetic_alphabet()
